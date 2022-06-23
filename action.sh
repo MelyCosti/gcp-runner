b55d00e00d311724a36bc0520e29d8ca714246d1
MelyCosti <105198589+MelyCosti@users.noreply.github.com>
2022-06-23 17:32:55 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -152,8 +152,9 @@ function start_vm {
   echo "The new GCE VM will be ${VM_ID}"
 
   startup_script="
-    gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=0 && \\
-    RUNNER_ALLOW_RUNASROOT=1 cd /actions-runner && ls && ./config.cmd --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --labels ${VM_ID} --unattended ${ephemeral_flag} --disableupdate --runasservice && \\
+    gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=0
+    RUNNER_ALLOW_RUNASROOT=1 cd /actions-runner 
+    ./config.cmd --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --labels ${VM_ID} --unattended ${ephemeral_flag} --disableupdate --runasservice
     gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=1
     # 3 days represents the max workflow runtime. This will shutdown the instance if everything else fails.
     echo \"gcloud --quiet compute instances delete ${VM_ID} --zone=${machine_zone}\" | at now + 3 days
